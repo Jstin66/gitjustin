a6a7761d4ec1717d6618852b290e25a61dc424f7
JUSTIN S. UCETA <JUSTIN S. UCETA@DESKTOP-PUIBVKT>
2025-04-29 17:51:20 -0400

    programa multiplicacion

--- programas basicos/programas basicos/Program.cs
+++ programas basicos/programas basicos/Program.cs
@@ -8,15 +8,15 @@ namespace programas_basicos
         {
             Console.WriteLine("=== División de dos números ===");
 
-            // Pedir el primer número
+            
             Console.Write("Ingresa el primer número: ");
             double numero1 = Convert.ToDouble(Console.ReadLine());
 
-            // Pedir el segundo número
+          
             Console.Write("Ingresa el segundo número: ");
             double numero2 = Convert.ToDouble(Console.ReadLine());
 
-            // Validar que el segundo número no sea cero
+       
             if (numero2 != 0)
             {
                 double resultado = numero1 / numero2;
