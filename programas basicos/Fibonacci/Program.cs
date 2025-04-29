ab733bd9940254030f7fec03ae9b2788da0f69a8
JUSTIN S. UCETA <JUSTIN S. UCETA@DESKTOP-PUIBVKT>
2025-04-29 18:33:36 -0400

    sucesion fibonacci

---
+++ programas basicos/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Serie de Fibonacci del 1 al 50:");
+
+        int a = 0;
+        int b = 1;
+
+        while (b <= 50)
+        {
+            Console.WriteLine(b);
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+    }
+}
+
