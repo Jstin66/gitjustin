1aa0b77fe9f5a82f10963f6a07f4fcabe4c5a5f4
JUSTIN S. UCETA <JUSTIN S. UCETA@DESKTOP-PUIBVKT>
2025-05-21 04:33:45 -0400

    Eliminando archivos temporales y aplicando .gitignore

---
+++ programas basicos/numeros desordenados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int[] numeros = new int[10];
+
+        Console.WriteLine("Ingresa 10 números desordenados:");
+
+        
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+
+          
+            while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+            {
+                Console.Write("Entrada inválida. Ingresa un número válido: ");
+            }
+        }
+
+       
+        Array.Sort(numeros);
+
+      
+        Console.WriteLine("\nNúmeros ordenados (ascendente):");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
